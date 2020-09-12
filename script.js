55cb77da83e8f7c535b471940352069d098a018c
Akhila C K <akhilackgopal@gmail.com>
2020-09-12 19:58:51 +0530

    challenge 4

--- script.js
+++ script.js
@@ -88,3 +88,51 @@ function rpsfrontEnd(humanImageChoice, botImageChoice, finalMessage){
     document.getElementById('flex-box-rps-div').appendChild(botDiv);
 
 }
+
+//Challenge 4:Change the color of all Buttons
+var all_buttons = document.getElementsByTagName('button');
+
+var copyAllButtons=[];
+for(let i=0;i<all_buttons.length;i++){
+    copyAllButtons.push(all_buttons[i].classList[1]);
+}
+
+function buttonColorChange(buttonThingy){
+    if (buttonThingy.value==='red'){
+        buttonRed();
+    }else if (buttonThingy.value==='green'){
+        buttonGreen();
+    }else if (buttonThingy.value==='reset'){
+        buttonColorReset();
+    }if (buttonThingy.value==='random'){
+        buttonColors();
+    }
+}
+
+function buttonRed(){
+   for(let i=0; i<all_buttons.length; i++){
+       all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+       all_buttons[i].classList.add('btn-danger');
+   }
+}
+function buttonGreen(){
+    for(let i=0; i<all_buttons.length; i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-success');
+    }
+ }
+
+ function buttonColorReset(){
+    for(let i=0; i<all_buttons.length; i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(copyAllButtons[i]);
+ }
+}
+function randomColors(){
+    var choices = ['btn-primary', 'btn-danger','btn-success','btn-warning']
+    for(let i=0; i<all_buttons.length; i++){
+       var randonNumber=Math.floor(Math.random()*4)
+       all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+       all_buttons[i].classList.add(choices[randonNumber]);
+    }
+}
